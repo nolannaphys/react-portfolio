cae01f8492d89157f98bc97b581ec2fbfb8faab9
Nolan <nolannaphys@gmail.com>
2023-08-14 17:54:26 -0400

    style updated

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,11 +5,22 @@ import './style/Contact.css'
 
 const Contact = () => {
     return (
-            <div class="contactDiv">
-                <h1 class="contactTitle">Contact Me</h1>
-                <h2 class="contactText">Email: nolannaphys@gmail.com</h2>
-                <h2 class="contactText contactBottom">Phone Number: (609) 502-1902</h2>
-            </div>
+        <div class="contactDiv">
+            <h1 class="contactTitle">Contact Me</h1>
+            <h2 class="contactText">
+                <a> Email: nolannaphys@gmail.com</a>
+            </h2>
+            <h2>
+                <a href='https://github.com/nolannaphys'>Github</a>
+            </h2>
+            <h2>
+                <a href='https://www.linkedin.com/in/nolan-naphys-633934194/'>LinkedIn</a>
+            </h2>
+            <h2>
+                <a href='https://stackoverflow.com/users/22389991/nolan-naphys'>Stack Overflow</a>
+            </h2>
+            <h2 class="contactText contactBottom"></h2>
+        </div>
     )
 }
 export default Contact
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,36 +1,38 @@
 import githubLogo from '../assets/github.svg.png'
 import './style/Project.css'
+import parkBrew from '../assets/park-brew.png'
+import critterTracker from '../assets/critter-tracker.png'
 
 export default function Project() {
     const projects = [
         {
-            name: "",
-            githubLink: "",
-            websiteLink: "",
-            description: "",
-            image: ""
+            name: "Park Brew",
+            githubLink: "https://github.com/nolannaphys/park-brew",
+            websiteLink: "https://nolannaphys.github.io/park-brew/",
+            description: "When the user inputs a city and state into our search bars a list of 5 National Parks and 5 Breweries will populate. The user can click on the different names provided to visit the website for the parks and breweries. If the user is not satisfied with the results they can continue to hit the search bar to see other results in the given location. If the user chooses to click the save icon they can store the results in client-side storage if they want to visit those locations in the future.",
+            image: parkBrew
         },
         {
-            name: "",
-            githubLink: "",
-            websiteLink: "",
-            description: "",
-            image: ""
+            name: "Critter Tracker",
+            githubLink: "https://github.com/nolannaphys/critter-tracker",
+            websiteLink: "https://critter-tracker-p2-d4b8f730eb59.herokuapp.com/",
+            description: "Critter Tracker is a platform that enables users to create profiles and share information about animals through pictures and descriptions. It utilizes a database to store and retrieve user data, including profiles and uploaded animal content.",
+            image: critterTracker
         },
-        {
-            name: "",
-            githubLink: "",
-            websiteLink: "",
-            description: "",
-            image: ""
-        },
-        {
-            name: "",
-            githubLink: "",
-            websiteLink: "",
-            description: "",
-            image: ""
-        },
+        // {
+        //     name: "",
+        //     githubLink: "",
+        //     websiteLink: "",
+        //     description: "",
+        //     image: 
+        // },
+        // {
+        //     name: "",
+        //     githubLink: "",
+        //     websiteLink: "",
+        //     description: "",
+        //     image: 
+        // },
     ]
     
 return(
