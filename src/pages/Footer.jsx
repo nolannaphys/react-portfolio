1d586252ba58e9b57bcbf2e79393ddb5bd5e76d4
Nolan <nolannaphys@gmail.com>
2023-10-02 09:54:38 -0400

    updated footer

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer(){
        <footer className=" footer d-flex justify-content-center p-5">
         <a href="https://github.com/nolannaphys"><img src={githubLogo} className="logo" alt="Github logo"></img></a>
         <a href="https://www.linkedin.com/in/nolan-naphys-633934194/"><img src={linkedinLogo} className="logo" alt="Linkedin logo"></img></a>
-        <a href="https://stackoverflow.com/users/22389991/nolan-naphys"><img src={stackOverflowLogo} className="logo" alt="Stack overflow logo"></img></a>
+        {/* <a href="https://stackoverflow.com/users/22389991/nolan-naphys"><img src={stackOverflowLogo} className="logo" alt="Stack overflow logo"></img></a> */}
        </footer>
     )
 };
